59f2811f9198bdec00ae1dd14f226c024adbf73c
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-06-28 22:40:18 +0200

    hub: install python2-koji too

--- hub/bin/build-koji.sh
+++ hub/bin/build-koji.sh
@@ -22,7 +22,7 @@ cd /opt/koji
 rm -rf noarch
 make test-rpm
 
-yum -y localinstall noarch/koji-hub*.rpm noarch/koji-1.*.rpm noarch/koji-web*.rpm
+yum -y localinstall noarch/python2-koji*.rpm noarch/koji-hub*.rpm noarch/koji-1.*.rpm noarch/koji-web*.rpm
 
 psql="psql --host=koji-db --username=koji koji"
 
